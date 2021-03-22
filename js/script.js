c851d25b085808aebc8ed4e814db1180564219d6
Michael <scocco.michael87@gmail.com>
2021-03-22 18:04:04 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -2,106 +2,85 @@
 // REPO: js-paliedispari
 // GOAL:
 
-// esercizio n°1 Una funzione per capire se la parola è palindroma
+// Una funzione per capire se la parola è palindroma
 
-/*l'ersercizio, prevede di creare un button ed un input text, dove il tutto prende vita dal momento in cui attivi il click,
-inoltre non si deve creare un array, con parole palindrome all'interno, ma si deve far in modo che il sistema
-prende atto che la parola stessa sia palindrama o meno, di conseguenza dato ke la parola è perfettamente identica leggendola al contrario
-la si deve dividere in due, e arrotondarla per negativo(es: 3/2 = 1,5 diventa 1), quindi al mio ciclo for, devo dirgli che deve leggere solo metà della parola
-da destra a sinistra, e che deve leggere l'latra metà da sinistra verso destra.*/
+// se l'utente inserisce una parola, la funzione capisce se è palindroma o meno;
 
-// tutto questo detto in una funzione (vedere la lezione di oggi per capire ke cazzo è una funzione)
 
-    // step-2, creo il mio prompt(), per chiedere all'utente di inserire una parola.
-    // var parolaUtente = prompt('scrivi una parola');
+var parolaUtente = prompt('dammi una parola palindroma');
 
-/*    var parolaBnt = document.getElementById('button');
-    parolaBnt.addEventListener('click', function() {
+function controllaParola(parolaUtente) {
 
-    var parolaUtente = document.getElementById('parolaInserita').value//contiene il valore dell'input, delutente inserito
+    var parolaData = parolaUtente.length;// conto la lunghezza dei caratteri della parola
+    console.log(parolaUtente + ' la parola è formata da numero caratteri ' + parolaData);
 
-    var lunghezza = parolaUtente.length;
+    var parolaRisulta = Math.floor(parolaData / 2);//divido la parola in due, metto il floor perchè voglio una divisione senza virgola
+    console.log('la parola una volta divisa risulta ' + parolaRisulta);
 
-    var esito = verificataPalindrome(parolaUtente);//variabile
+    for (var i = 0; i < parolaRisulta; i++) {
 
-    if (esito) {
-        console.log('la parola è palindroma se è true');
-
-    }else {
-        console.log('la parola non è palindroma se è false');
-
-    }
-
-});
-
-    function verificataPalindrome(parola){//parametro
-
-        var lunghezza;
-
-        var giriLoop = Math.floor(lunghezza / 2);
-
-        var risultato;
-        for (var i = 0; i < giriLoop; i++) {
-
-            var primaLettera = parolaUtente[i];
-            var ultimaLettera = parolaUtente[parolaUtente.length - 1 - i];
-
-            console.log(primaLettera, ultimaLettera);
-            if (primaLettera === ultimaLettera) {
-                risultato = true;
-            }else {
-                risultato = false;
-                break;
-            }
+        if (parolaUtente.charAt(i) !== parolaUtente.charAt(parolaData - 1 - i) ) {
+            return false;
         }
-        console.log(risultato);
-        return risultato;
-    }*/
+    }
+    return true;
+}
+var risultaVero = controllaParola(parolaUtente);
+console.log(risultaVero);
 
-// _________________________________________________________________________________
+// _____________________________________________________________________________
 
-// esercizio n°2 L'utente sceglie pari o dispari e un numero
-// da 1 a 5.
-    // step-1 creare due prompt, dove chiediamo prima il pari e poi un numero all'utente
-    var pariDispari = prompt('dimmi se vuoi il pari o dispari');
-    var numero = parseInt(prompt('scegli un numero tra 1 e 5'));
-    console.log(pariDispari, numero);
-
-    var sceltaGiocatore = pariDispari + ' ' + numero;
-
+// esercizio n°2
+// L'utente sceglie pari o dispari e un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer.
-    // step-2 creiamo una random, ma la facciamo diventare una funzione, cosi da usarla sempre in futuro.
-
-    function generatoreRandom(min, max) {
-
-    var minRnd = min;
-    var maxRnd = max - minRnd + 1;
-
-    var random1 = Math.floor(Math.random() * maxRnd) + minRnd;
-
-    return random1;
-    }
-
-var risultatoRandom = generatoreRandom(1, 5);
-console.log(risultatoRandom +' numero pc');
-
-
-
 // stabiliamo se la somma dei due numeri è pari o dispari. Sommiamo i due numeri e dichiariamo chi ha vinto.
-    // step-3 se la somma che esce tra l'utente ed il pc è pari o dispari, si deve decidere chi ha vinto in base al prompt della scelta dell'utente.
 
-var somma = numero + risultatoRandom;
-console.log(somma);
 
-var x = somma % 2;//variante per il pari e dispari
+var sceltaUtente = prompt('cosa scegli');
+console.log('l\'utente ha scelto ' + sceltaUtente);
 
-    if(x == 0){
-        console.log('pari' + ' win');
+function generaNumeriCasuali(min, max) {
 
+    var valoreMin = min;
+    var valoreMax = max - valoreMin +1;
+
+    var numeroCasuale = Math.floor(Math.random() *valoreMax ) + 1;
+
+    return numeroCasuale;
+}
+
+function giocoPariDispari(sceltaUtente) {
+
+    var numeroCasualeGiocatore = generaNumeriCasuali(1, 5)
+    console.log('numero giocatore ' + numeroCasualeGiocatore);
+
+    var numeroCasualePc = generaNumeriCasuali(1, 5)
+    console.log('numero pc ' + numeroCasualePc);
+
+    var somma = numeroCasualeGiocatore + numeroCasualePc;
+    console.log('il numero del giocatore ' + numeroCasualeGiocatore +
+    ' + ' + numeroCasualePc + ' il numero del pc, è = ' + somma);
+
+
+    var risultato;
+
+    if (somma % 2 == 0 && sceltaUtente === 'pari') {
+        risultato = true;
+        console.log(risultato + ' il numero è pari l\'utente ha vinto ha scelto ' + sceltaUtente);
+    }else if (somma % 2 == 1 && sceltaUtente === 'dispari') {
+        risultato = true;
+        console.log(risultato + ' il numero è dispari l\'utente ha vinto ha scelto ' + sceltaUtente);
+    }else if (sceltaUtente === somma % 2 == 1) {
+        risultato = false;
+        console.log(risultato + ' l\'utente ha perso ha ha scelto dispari');
     }else {
-        console.log('dispari' + ' win');
-
+        risultato = false;
+        console.log(risultato + ' l\'utente ha perso ha scelto pari');
     }
 
-var stampaHtml = document.getElementById('pariOdispari');
-stampaHtml.innerHTML = somma + ' il vincitore è ' + sceltaGiocatore;
+return risultato;
+}
+
+var risultato = giocoPariDispari(sceltaUtente)
+console.log(risultato);
+// _____________________________________esercizi per casa parola palindroma-->
