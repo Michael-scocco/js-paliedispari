a6d064afbc62825cc223ed7cfa4523526f0451b8
Michael <scocco.michael87@gmail.com>
2021-04-29 22:18:40 +0200

    esercizi finiti
    palindroma, fatta con moduli, while e for.... pari e dispari finito, ma manca la funzione dei disp/par

--- js/script.js
+++ js/script.js
@@ -12,13 +12,10 @@ Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andia
 Se non sappiamo cos'é una parola palindroma Googliamo!
 */
 
-
-
-
-
-
-// 1. Chiedere all’utente di inserire una parola
-    var parolaUtente = prompt('vediamo se è palindroma');
+/************************************************
+// 1. Chiedere all’utente di inserire una parola*
+************************************************/
+    // var parolaUtente = prompt('vediamo se è palindroma');
 
 /*
 // 1.1 facciamo in modo che qualunque parola sia sempre in minuscolo
@@ -61,9 +58,11 @@ if(parolaUtente == parolaInvertita){
 }
 */
 
-// versione 1 FUNZIONA
+/*****************************
+// VERSIONE 1 metodi separati*
+*****************************/
 /*
-function parolaPalindroma(inserisciParola){
+function parolaPalindroma1(inserisciParola){
 
     inserisciParola = inserisciParola.toLowerCase();
 
@@ -78,18 +77,22 @@ function parolaPalindroma(inserisciParola){
     }
     return 'non è palindroma';
 }
-var palindroma = parolaPalindroma(parolaUtente);
-console.log(palindroma);
+
+// var palindroma = parolaPalindroma(parolaUtente);
+// console.log(palindroma);
 */
 
-//  VERSIONE 2 non funziona devo capire il perchè
+/******************************
+// VERSIONE 2 metodi COMPRESSI*
+******************************/
 
-function parolaPalindroma(inserisciParola){
+/*
+function parolaPalindroma2(inserisciParola){
 
-    inserisciParola = inserisciParola.toLowerCase().split('').reverse().join('');
+    var verifica = inserisciParola.toLowerCase().split('').reverse().join('');
 
     var risultato = '';
-    if (inserisciParola != inserisciParola) {
+    if (verifica != inserisciParola) {
         risultato = 'non è palindroma';
 
     }else {
@@ -98,18 +101,77 @@ function parolaPalindroma(inserisciParola){
     }
 return risultato;
 }
+// var palindroma = parolaPalindroma2(parolaUtente);
+// console.log(palindroma);
+*/
 
 
+/***************************
+//  VERSIONE 3 versione FOR*
+***************************/
 
+/*
+function parolaPalindroma3(parolaUtente) {
 
+    // devo fare un ciclo che rovescia la parola, quindi un for inverso.
+    var parola = '';//inizializzo la variabile per concatenare i caratteri
 
-// versione for
-// devo fare un ciclo che rovescia la parola, quindi un for inverso.
+    for (var i = parolaUtente.length - 1; i >= 0; i--) {
+        var singoloCarattere = parolaUtente[i]
 
-for (var i = 0; i < parolaUtente.length; i++) {
-    parolaUtente[i]
+        // devo salvare questa parola in una variabile, la devo concatenare dentro
+        parola += singoloCarattere;
+        console.log(`lettera che viene concatenata ${parola}, ${singoloCarattere} questo è il singolo carattere`);
+
+    }
+    // adesso ke ho concatenato e salvato la parola in una stringa, posso confrontarla
+    if (parola === parolaUtente) {
+        parola = ' è palindroma';
+
+    }else {
+        parola = ' non è palindroma';
+    }
+ return 'risultato ' + parola;
 }
 
+var palindromaFor = parolaPalindroma3(parolaUtente);
+console.log(palindromaFor);
+*/
+
+/*****************************
+//  VERSIONE 4 versione while*
+*****************************/
+
+/*
+function parolaPalindroma4(parolaUtente) {
+
+    var i = parolaUtente.length - 1;
+
+    var parola = '';
+
+    while (i >= 0) {
+
+        var singoloCarattere = parolaUtente[i];
+
+        parola += singoloCarattere;
+        console.log(`lettera che viene concatenata ${parola}, ${singoloCarattere} questo è il singolo carattere`);
+
+        i--;
+    }//while
+
+    // adesso ke ho concatenato e salvato la parola in una stringa, posso confrontarla
+    if (parola === parolaUtente) {
+        parola = ' è palindroma';
+
+    }else {
+        parola = ' non è palindroma';
+    }
+return 'risultato ' + parola;
+}//parolaPalindroma4
+
+var palindromaWhile = parolaPalindroma4(parolaUtente);
+console.log(palindromaWhile);
+*/
 
 /*
 Pari e Dispari:
@@ -122,3 +184,62 @@ Scriviamo sempre in italiano i passaggi che vogliamo fare
 Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 Se non sappiamo cos'é una parola palindroma Googliamo!
 */
+
+// 1. L’utente sceglie pari o dispari
+var sceltaUtente = prompt('pari o dispari');
+
+// 1.1 Inserisce un numero da 1 a 5
+var numeroUtente = Number(prompt('dammi un numero tra 1 e 5'));
+console.log(numeroUtente, sceltaUtente);
+
+// 2.0 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+function generatoreRandom(min, max) {
+
+    return Math.floor(Math.random()* (max - min + 1) + min);
+
+}
+var randomPc = generatoreRandom(1, 5);
+console.log(randomPc);
+
+// 3.0 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+var sum = numeroUtente + randomPc;
+console.log('questa è la somma ' + sum);
+/*
+function PariDispari(numeroUtente, randomPc) {
+
+    var sum = numeroUtente + randomPc;
+    console.log('questa è la somma ' + sum);
+
+    var risultato = '';
+
+    if (sum % 2 == 0) {
+        risultato = ' pari';
+        // console.log(risultato);
+    }else {
+        risultato = ' dispari';
+        // console.log(risultato);
+    }
+
+return risultato;
+}
+var risultato = PariDispari(numeroUtente, randomPc);
+console.log(risultato);
+*/
+// 4.0 Dichiariamo chi ha vinto.
+var vince = '';
+
+if (sum % 2 == 0 && sceltaUtente === 'pari') {
+    vince = `ha vinto il player con il risultato pari ed il numero che ha scelto era ${numeroUtente}`;
+
+}else if (sum % 2 == 1 && sceltaUtente === 'dispari') {
+    vince = `ha vinto il player con il risultato dispari ed il numero che ha scelto era ${numeroUtente}`;
+
+}else if (sceltaUtente === 'dispari' && sum % 2 == 0 ) {
+    vince = `ha vinto il pc con il risultato pari ed il numero era ${randomPc}`;
+
+}else if (sceltaUtente === 'pari' && sum % 2 == 1) {
+    vince = `ha vinto il pc con il risultato dispari ed il numero era ${randomPc}`;
+}
+console.log(vince);
